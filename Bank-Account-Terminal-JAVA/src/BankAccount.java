b439f87258866f200a2b2ce4894e26fc08afa00d
joaolotjr <joaolotjr@gmail.com>
2023-01-12 16:14:37 -0300

    Completed project

---
+++ Bank-Account-Terminal-JAVA/src/BankAccount.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BankAccount {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome:");
+        String nome = sc.nextLine();
+        
+        System.out.println("Digite seu Número da Conta:");
+        int numero = sc.nextInt();
+
+        System.out.println("Digite sua Agência:");
+        String agencia = sc.next();
+               
+        System.out.println("Digite seu Saldo:");
+        double saldo = sc.nextDouble();
+
+
+        System.out.println("Ola " + nome + " brigado por criar uma conta em nosso banco, sua agência é " + agencia + " ,conta " + numero + " e seu saldo de " + saldo + " reais já está disponível para saque." );
+
+
+
+
+    }
+}
